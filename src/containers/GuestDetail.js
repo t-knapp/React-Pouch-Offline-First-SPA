9f46804a648600e2b9936e2ab7a059a037b5f7b0
t-knapp <tobias-knapp@web.de>
2018-04-02 21:24:14 +0200

    Details from redux store.

--- src/containers/GuestDetail.js
+++ src/containers/GuestDetail.js
@@ -1,5 +1,15 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 
+import _ from 'lodash';
+
+@connect((store) => {
+    return {
+        guests: store.GuestReducer.guests
+    }
+})
+@withRouter
 export default class GuestDetail extends Component {
 
     constructor(params) {
@@ -8,13 +18,30 @@ export default class GuestDetail extends Component {
     }
 
     render() {
-        console.log(this.props);
-        const guestId = this.props.match.params.guestId;
+        const guestId = parseInt(this.props.match.params.guestId);
+        /*
+        const guest = _.find(this.props.guests, function(item) {
+            return item.guestId === guestId;
+        });
+        */
+        //const guest = _.find(this.props.guests, {'guestId': guestId});
+        const guest = _.find(this.props.guests, ['guestId', guestId]);
         return (
             <div>
                 <h2>GuestDetail: {guestId}</h2>
+                <p>
+                    Name: {guest.name}
+                </p>
+                <p>
+                    Lastname: {guest.lastname}
+                </p>
+                <p>
+                    Section: {guest.section}
+                </p>
+                <p>
+                    State: {guest.state}
+                </p>
             </div>
         );
     }
-
 }
